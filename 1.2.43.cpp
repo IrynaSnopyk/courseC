a7f9beebfeaa1e37e2281ddfaf6e7e900251d8f3
IrynaSnopyk <59543109+IrynaSnopyk@users.noreply.github.com>
2020-11-15 14:28:50 +0300

    Add files via upload
    dz5: 1.2.27a;  1.2.43;
    dz6: 1.2.45o;  1.2.40б

---
+++ 1.2.43.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Input number: ";
+    cin >> number;
+    int number1 = number;
+    int result = 0;
+
+    while (number1 != 0) {
+        number1 /= 10;
+        result++;
+    }
+
+    int a, y_n, y_n1;//y(n); y(n-1)
+    y_n1 = 0;
+    for (int i = 1; i <= result; i++) {
+        int powe = pow(10,i);
+        a = number % powe / pow(10, i - 1);
+        y_n = y_n1 * 10 + a;
+        y_n1 = y_n;
+    }
+    cout << "Result: " << y_n;
+}
+
---
+++ 1.2.45o.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float x;
+	cin >> x;
+	if (abs(x) >= 1) {
+		cout << "Mistake";
+	}
+	else {
+		float sum, sum_1, sum0 = 0;
+		float a_k, a_k1=x;
+		sum_1 = sum0+a_k1;
+		int i = 2;
+		do {
+			a_k = ((2 * i - 1) * x * x*a_k1) / (2 * i)*(2 * i)*(2 * 1 + 1);
+			sum = sum_1 + a_k;
+			sum_1 = sum;
+			a_k1 = a_k;
+			i++;
+			//cout << sum << "\n" << a_k << "\n";
+		} while (abs(a_k)>FLT_EPSILON);
+		cout << "y = arcsin x = " << sum;
+
+	}
+}
