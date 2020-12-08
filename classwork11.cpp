4a8aee045443672dca88dbdb0a1f3fa5877ffd5a
IrynaSnopyk <59543109+IrynaSnopyk@users.noreply.github.com>
2020-12-08 17:18:15 +0200

    IrynaSnopyk-dz23,classwork23

---
+++ classwork11.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Input count of people: ";
+	cin >> n;
+	int t;
+	queue<int>q1;
+	for (int i = 0; i < n; i++) {
+		cout << "time "<<i+1<<" person: ";
+		cin >> t;
+		q1.push(t);
+	}
+
+	vector <int>c;
+	int time = 0;
+	while (!q1.empty()) {
+		t = q1.front();
+		q1.pop();
+		time += t;
+		c.push_back(time);
+	}
+	cout << endl;
+	for (vector<int>::iterator it = c.begin(); it != c.end(); ++it) {
+		cout << *it << ", ";
+	}
+}
---
+++ dz23.15.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+#include <queue>
+using namespace std;
+
+struct BlackBox {
+	int K = 0;
+	queue<int>q1;
+	void pushh(int t) {
+		q1.push(t);
+		K++;
+	}
+	int  min_k() {
+		int min_value = INT_MAX;
+		size_t size = q1.size();
+		while (size-- > 0) {
+			int x = q1.front();
+			q1.pop();
+			q1.push(x);
+			if (x < min_value)
+				min_value = x;
+		}
+		return min_value;
+	}
+
+};
+int main() {
+	BlackBox r;
+	r.pushh(6);
+	r.pushh(5);
+	cout << r.K << endl;
+	cout<<r.min_k();
+}
